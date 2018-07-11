edc56066c458e9fd3f0ad0d1396a0c7a6957f7c1
argirowski <argirovski.g@gmail.com>
2018-07-11 20:50:33 +0200

    simple exercises regarding functions

---
+++ JSfunctionExercise/functions.js
@@ -0,0 +1,30 @@
+// isEven
+
+// function isEven(num) {
+// 	if (num % 2 === 0) {
+// 		return true;
+// 	} else {
+// 		return false;
+// 	}
+// }
+
+function isEven(num) {
+	return num % 2 === 0;
+}
+
+// factorial
+
+function factorial(num) {
+	var result = 1;
+	for (var i = 2; i <= num; i++) {
+		result *= i;
+	}
+	return result;
+}
+
+// kebabToSnake
+
+function kebabToSnake(str) {
+	var newStr = str.replace(/-/, "_");
+	return newStr;
+}
