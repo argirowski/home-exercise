d8784bacf8dacb68db0b613ea733bd9323b11730
argirowski <argirovski.g@gmail.com>
2019-01-14 19:02:57 +0100

    deleting an unnecessary folder

--- semosProjectzzzzzz/handlers/root.js
+++
@@ -1,3 +0,0 @@
-module.exports = (req, res) => {
-    res.send("Version in Progress");
-}
--- semosProjectzzzzzz/utils/index.js
+++
@@ -1,7 +0,0 @@
-var isEmpty = (v) => {
-    return v === undefined || v === "" || v === null
-}
-
-module.exports = {
-    isEmpty
-}
